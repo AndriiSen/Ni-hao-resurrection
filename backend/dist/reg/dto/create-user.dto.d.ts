0deba28ddc7f188eda0eee4143d2fd452a0ddb9b
AlexandrKaplyukhin <kaplyukhin7@gmail.com>
2021-10-18 21:54:53 +0300

    Removed folder from repository

--- backend/dist/reg/dto/create-user.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class createUserDto {
-    readonly login: string;
-    readonly email: string;
-    readonly password: string;
-    readonly date: string;
-}
--- backend/src/reg/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Date } from "mongoose";
-
-export class createUserDto {
- 
-  readonly login: string;
-  readonly email: string;
-  readonly password: string;
-  readonly date: string;
-}
