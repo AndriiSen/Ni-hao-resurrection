986ce5f90085280077c33374a15d5aa47dabef5d
Andrii Sen <senanven@gmail.com>
2021-10-26 22:20:48 +0300

    feat: add passwords compare reature

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { User } from "src/users/schemas/user.schema";
 import { UsersRepository } from "src/users/users.repository";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,6 +18,10 @@ export class AuthService {
     async validateUser(login: string, password: string): Promise<any> {
         const user = await this.usersRepository.findOne({login});
         if(!user) {
+            throw new UnauthorizedException()
+        } 
+        const passwordIsValid = await bcrypt.compare (password, user.password)
+        if (!passwordIsValid) {
             throw new UnauthorizedException()
         }
         return user
