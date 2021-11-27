b1ffadb29b3a1f381ccc8180ee6428b3a8277e16
Andrii Sen <senanven@gmail.com>
2021-11-27 14:43:58 +0200

    feat: user profile-link bottom

--- frontend/src/app/_helpers/interceptors/jwt-interceptor.service.ts
+++ frontend/src/app/_helpers/interceptors/jwt-interceptor.service.ts
@@ -11,7 +11,7 @@ export class JwtInterceptorService implements HttpInterceptor {
   intercept(req: any, next: any) {
     let tokenizedReq = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${localStorage.getItem('Auth-Token')}`;
+        Authorization: `Bearer ${localStorage.getItem('Auth-Token')}`
       }
     })
     return next.handle(tokenizedReq);
